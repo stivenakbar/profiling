4a16400744b0aa55bee2c958496155b488c5d604
LAPTOP-CRC0LPJD\LENOVO <l200210226@student.ums.ac.id>
2024-02-20 20:46:47 +0700

    first commit

---
+++ mahasiswa-project - Copy/resources/views/mahasiswa.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+
+<head>
+
+
+<meta charset="UTF-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Data Diri</title>
+</head>
+<body>
+<ol>
+
+<h1>Data Diri</h1>
+
+<li>Nama: Stiven Akbar Firmansyah</li>
+<li>NIM: L200210226</li>
+<li>PRODI: Informatika </li>
+<li>Fakultas: FKI </li>
+<li>Jenis Kelamin: Laki-laki </li>
+
+
+</ol>
+
+ </body>
+
+ </html>
