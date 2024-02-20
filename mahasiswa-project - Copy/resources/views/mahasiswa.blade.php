45710ca68a597932a4e2f457027c21f41a1cbf95
LAPTOP-CRC0LPJD\LENOVO <l200210226@student.ums.ac.id>
2024-02-20 20:56:09 +0700

    mengganti judul

--- mahasiswa-project - Copy/resources/views/mahasiswa.blade.php
+++ mahasiswa-project - Copy/resources/views/mahasiswa.blade.php
@@ -12,7 +12,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Data Diri</title>
+<title>Data Diri mahasiswa</title>
 </head>
 <body>
 <ol>
